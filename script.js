03c1e580388d0271b30f917a568633cf5782c528
mourad zamoun <mourad.zamoun@gmail.com>
2021-04-29 00:34:07 +0200

    only page1 work

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-  function gotoUrl(site) {
+site='file:///home/mourad/data/pro/projets_html/dr-n-benkari/'
+
+function gotoUrl(site) {
     //location.replace(site)
     //window.location.href = site;
 //       window.close();
 //       window.open(site);
     window.location.replace(site);
   }
+    //////////  
+    function loadIframe(idDiv, file) {
+
+      $(idDiv).height($(window).height());
+      $(idDiv).width($(window).width());
+
+      $( window ).resize(function() {
+           $(idDiv).height($(window).height());
+           $(idDiv).width($(window).width());
+       });
+
+      $(idDiv).attr('src' , file);
+
+
+     }
   //////////
   function showAcc() {
-     gotoUrl('./index.html');
+     //gotoUrl('./index.html');
+     $('#Pages').hide();
+     $('#Acc').show();
   }
   //////////
   function showPage(page) {
-     gotoUrl('./'+ page + '.html');
+     page = './'+ page + '.html';
+     $('#Acc').hide();
+     loadIframe('#fpage', page);
+     $('#Pages').show();
+
   }
-  //////////  
+///////////////
