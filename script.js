f0e015b9ab66f34cff1568f58c941fa5406a02e0
Mourad ZAMOUN <mourad.zamoun@gmail.com>
2021-04-28 20:37:39 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+  function gotoUrl(site) {
+    //location.replace(site)
+    //window.location.href = site;
+//       window.close();
+//       window.open(site);
+    window.location.replace(site);
+  }
+  //////////
+  function showAcc() {
+     gotoUrl('./index.html');
+  }
+  //////////
+  function showPage(page) {
+     gotoUrl('./'+ page + '.html');
+  }
+  //////////  
